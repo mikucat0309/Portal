9b6d220b2899f5cf8326680793586392c9600dfd
mikucat0309 <admin@mikuc.at>
2023-08-26 10:45:56 +0000

    docs: improve event schema descriptions

--- event_schema.js
+++ event_schema.js
@@ -5,42 +5,45 @@
     "zh": "",
     "<Any ISO 639-1 lang>": ""
   },
-  "logo_url": "https://", // only accept HTTPS URL
-  "event_website": "https://", // please use HTTPS URL
-  "event_date": { // only accept ISO 8601
+  "logo_url": "https://", // logo URL
+  "event_website": "https://", // website URL
+  "event_date": { // ISO 8601 datetime
     "start": "2020-03-28T00:00:00+08:00",
     "end": "2020-03-28T00:00:00+08:00"
-  }
-  "publish": { // only accept ISO 8601
+  },
+  "publish": { // ISO 8601 datetime
     "start": "2020-02-23T00:00:00+08:00",
     "end": "2020-08-31T00:00:00+08:00"
   },
-  "features": [ // app follow the sequence
+  "features": [ // app will follow the sequence
     {
-      "feature": "", // fastpass, schedule, announcement, puzzle, ticket, telegram, im, sponsors, staffs, venue, webview, wifi
-      "icon": "https://" // require when feature is webview, only accept @3x.png max: 128x128
+      "feature": "", // fastpass, announcement, ticket, schedule, puzzle, telegram, im, sponsors, staffs, venue or webview
+      "icon": "https://", // optional icon URL, will override default icons
       "display_text": {
         "en": "",
         "zh": "",
         "<Any ISO 639-1 lang>": ""
       },
-      "url": "https://",
       /*
-       * only accept HTTPS URL, support placeholder {role}, {public_token}
-       * require when feature is fastpass, announcement or ticket, the url is server base url, https://<domain>
+       * support placeholder {role}, {token}, {public_token}
+       * the URL need to point to a valid CCIP server when feature is fastpass, announcement or ticket
+       * no effects when feature is wifi
        */
-      "wifi": [ // require when feature is wifi
+      "url": "https://",
+      // required when feature is wifi
+      "wifi": [
         {
-          "SSID": "SITCON 2020",
+          "SSID": "",
           "password": ""
         },
-        ...
-      ]
-      "visible_roles": [ // option, default for all user, string need match server config
+        // ...
+      ],
+      // optional, default for all user, string need match server config
+      "visible_roles": [
         "staff",
         "speaker"
       ]
     },
-    ...
+    // ...
   ]
 }
